4b0b6ad5092749dd1e806924b2b0031dca650eb3
ricardoparizotto <ricardo.dparizotto@gmail.com>
2023-06-13 20:34:21 +0000

    automating hosts communication and using a phold model

--- application.py
+++ application.py
@@ -1,22 +1,48 @@
 from shim_layer import *
+import numpy as np
+
+import os.path
+
+path = './example.txt'
+
+check_file = os.path.isfile(path)
+
+
+
 
 
 #TODO: Create models based on Phold
+nodes = {1: "10.0.1.1", 2: "10.0.2.2", 4: "10.0.4.4", 5: "10.0.5.5", 6: "10.0.6.6", 7: "10.0.7.7", 8: "10.0.8.8"}
+
+
+def starting():
+    i = False
+    while i==False:
+        for i in nodes:
+            i = os.path.isfile("shared_mem/"+str(i)+".txt")
+            if i == False:
+                break
+
 
 def main():
-
-    if len(sys.argv)<2:
-        print('pass 2 arguments: <destination> <process_id>')
+    if len(sys.argv)<1:
+        print('pass 1 argument: <process_id>')
         exit(1)
 
-    addr = socket.gethostbyname(sys.argv[1])
-    pid = int(sys.argv[2])
+    pid = int(sys.argv[1])
 
     shim = shim_layer(pid)
 
-    while True:
-        x = input()
-        shim.send(addr, input=x)
+    f = open("shared_mem/"+str(pid)+".txt", "w")
+
+    starting()
+
+    for node_id in nodes:
+        if(node_id != pid):
+            random_variable = np.random.uniform(0, 1)
+            if(random_variable > 0.5):
+                shim.send(nodes[node_id], input='x')
+                print("%d -> %d" %( pid, node_id ))
 
 
 if __name__ == '__main__':
---
+++ run.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import pexpect
+import argparse
+import subprocess
+import os
+import sys
+
+MAKEFILE_PATH = "/home/p4/tutorials/exercises/Resist-Bmv2/Makefile"
+
+class MininetProc:
+    def __init__(self) -> None:
+        self.proc = pexpect.spawn("make run", cwd=os.path.dirname(MAKEFILE_PATH), encoding="utf-8")
+        self.proc.logfile_read = sys.stdout
+
+    def run_coordinator(self):
+        self.proc.expect("mininet> ", timeout=None)
+        self.proc.sendline(f"h3 python3 coordinator.py &")
+
+    def run_server(self, id):
+        self.proc.expect("mininet> ", timeout=None)
+        self.proc.sendline(f"h"+str(id)+" python3 application.py "+str(id)+" &")
+
+    def wait(self):
+        self.proc.expect("mininet> ", timeout=None)
+        x = input()
+
+
+def main():
+    mininet_proc = MininetProc()
+    mininet_proc.run_coordinator()
+
+    for i in range(1,9):
+        if(i != 3):  #3 is the coordinator
+            mininet_proc.run_server(id=i)
+
+    mininet_proc.wait()
+
+if __name__ == "__main__":
+    main()
